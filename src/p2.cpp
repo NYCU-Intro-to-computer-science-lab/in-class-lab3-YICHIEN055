ded2c42f3d46fb4560899166ac7f60f6ca1cec72
YICHIEN055 <lyq.mg14@nycu.edu.tw>
2025-11-25 14:04:43 +0800

    GUI 自動提交

--- src/p2.cpp
+++ src/p2.cpp
@@ -3,8 +3,22 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+
+    int gcd(int a, int b)
+    {
+    if(b==0)
+     return a;
+    return gcd(b,a%b);
+}   
+    
+    int main()
+    {
+        int a,b;
+        cin>>a>>b;
+        cout<<gcd(a,b);
+
+        return 0;
+    }
+
     
     
-    return 0;
-}
